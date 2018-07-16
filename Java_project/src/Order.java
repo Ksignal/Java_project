a05f2514ecbbcf753706e37006c33d7e733f5fd3
Gaoxiangyuan <since_09110@163.com>
2018-07-16 11:22:10 +0800

    .

---
+++ Java_project/src/Order.java
@@ -0,0 +1,44 @@
+
+public class Order {
+	private int tableNum;
+	private String orderedMenu;
+	private int price;
+	private int orderedNum;
+		
+	public int getTableNum() {
+		return tableNum;
+	}
+
+	public void setTableNum(int tableNum) {
+		this.tableNum = tableNum;
+	}
+
+	public String getOrderedMenu() {
+		return orderedMenu;
+	}
+
+	public void setOrderedMenu(String orderedMenu) {
+		this.orderedMenu = orderedMenu;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getOrderedNum() {
+		return orderedNum;
+	}
+
+	public void setOrderedNum(int orderedNum) {
+		this.orderedNum = orderedNum;
+	}
+
+	public void getTotalPrice() {
+		
+	}
+
+}
